d16d4276c0160cb60f9fef42ac27012b0569c415
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2022-11-17 20:48:33 +0000

    Commented out style file import

--- Accordion/src/App_v1.1.0.jsx
+++ Accordion/src/App_v1.1.0.jsx
@@ -1,5 +1,5 @@
 import React, {useRef, useEffect} from "react";
-import "./App.css"
+//import "./App.css"
 
 export default function App() {
   //state is managed by redux hence no component state specified
