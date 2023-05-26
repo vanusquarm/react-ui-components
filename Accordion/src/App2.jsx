aa7c1feed4ca85382ceda43698bafa678505cbfb
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2023-05-26 13:52:54 +0000

    Create App2.jsx

---
+++ Accordion/src/App2.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export function App() {
+  
+  const sections = [
+    {summary: "section 1", detail: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"},
+    {summary: "section 2", detail: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"},
+    {summary: "section 3", detail: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+  ]
+
+  function showDetails(e){
+    e.currentTarget.classList.toggle("active");
+    e.currentTarget.nextElementSibling.classList.toggle("active");
+ 
+  }
+    
+  return (
+    <>            
+      <h2>Accordion</h2>
+      
+      {sections.map((section, i) => {
+          
+        return (
+          <>
+            <button key={i} className="accordion" onClick={(e) => showDetails(e)}>{section.summary}</button>
+            <div className="panel"> <p>{section.detail}</p> </div>
+          </>
+        );
+      })}
+
+    </>
+  );
+}
