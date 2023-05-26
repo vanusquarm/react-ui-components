9b93c689d3c9a7d08c369925e87ccdb636ec16d4
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2023-05-26 14:04:15 +0000

    Create data.js

---
+++ Accordion/src/data.js
@@ -0,0 +1,5 @@
+export const sections = [
+    {summary: "section 1", detail: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"},
+    {summary: "section 2", detail: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"},
+    {summary: "section 3", detail: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+]
