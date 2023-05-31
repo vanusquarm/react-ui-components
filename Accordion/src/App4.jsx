29ce021f4420b69bb9c13b4b79b1f9a4fd6e3285
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2023-05-31 10:26:45 +0000

    fix: map elements should have unique keys

--- Accordion/src/App4.jsx
+++ Accordion/src/App4.jsx
@@ -17,10 +17,10 @@ function Accordion({sections}) {
       {sections.map((section, i) => {
           
         return (
-          <>
-            <button key={i} className="accordion" onClick={(e) => showDetails(e)}>{section.summary}</button>
+          <div key={i}>
+            <button className="accordion" onClick={(e) => showDetails(e)}>{section.summary}</button>
             <div className="panel"> <p>{section.detail}</p> </div>
-          </>
+          </div>
         );
       })}
 
