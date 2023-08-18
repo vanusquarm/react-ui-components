f00ffc3b1362e58e88d661095febb1330cca2709
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2023-08-18 19:56:49 +0000

    Create TabsBody.jsx

---
+++ Tabs/src/TabsBody.jsx
@@ -0,0 +1,22 @@
+/**
+ * Created by sylvan quarm on 6/26/2023.
+ * https://github.com/vanusquarm/react-ui-components
+ */
+export default function TabsBody({tabId}){
+  // Using the first approach
+  const cities = [
+    {"id":1,"name":"London","details":"London is the capital tab of England."},
+    {"id":2,"name":"Paris","details":"Paris is the capital tab of France."},
+    {"id":3,"name":"Tokyo","details":"Tokyo is the capital tab of Japan."}
+  ]
+  let tab = cities.filter(tab => tab.id === tabId)[0]; 
+  console.log(tab)
+
+  return(
+    <div>
+        <h3>{tab.name}</h3>
+        <p>{tab.details}</p>
+    </div>
+  );
+
+}
