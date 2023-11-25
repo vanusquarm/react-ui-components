616d2af36221c277c5010fb1aa33ff49e6652fbf
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2023-11-25 23:04:51 +0000

    Update Tabs2.jsx

--- Tabs/src/Tabs2.jsx
+++ Tabs/src/Tabs2.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import TabsBody from './TabsBody';
-import TabsHeader from './TabsHeader';
+import React, { useState } from 'react';
 /**
  * Created by sylvan quarm on 6/26/2023.
  * https://github.com/vanusquarm/react-ui-components
@@ -10,30 +8,30 @@ export default function Tabs(props) { // Refactor name to Tab and Tab to TabsHea
   const [selectedTab, setSelectedTab] = useState(1);
   
 
-  const cities = [
+  const tabList = [
     {"id":1,"name":"London","details":"London is the capital tab of England."},
     {"id":2,"name":"Paris","details":"Paris is the capital tab of France."},
     {"id":3,"name":"Tokyo","details":"Tokyo is the capital tab of Japan."}
   ]
 
-  const tabBody = cities.filter(tab => tab.id === selectedTab)[0];
+  const tab = tabList.filter(tab => tab.id === selectedTab)[0];
 
   return (
  
     <div className='container'>
       <div className="tab">
-        {cities.map((tab, i) => {
+        {tabList.map((tab, i) => {
 
           return <div key={i} onClick={() => setSelectedTab(tab.id)} className={(tab.id === selectedTab) ? "active":""}>{tab.name}</div>
         })
         }
       </div>
       
-      <div className="tabcontent">
+      <div className="tab-panel">
         
         {<div>
-            <h3>{tabBody.name}</h3>
-            <p>{tabBody.details}</p>
+            <h3>{tab.name}</h3>
+            <p>{tab.details}</p>
         </div> }
       </div>      
       
